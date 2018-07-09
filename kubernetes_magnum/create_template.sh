fcb6e680092ce9cb08892840b09b609084ec5419
Aaron Culich <aculich@gmail.com, aculich@berkeley.edu>
2018-07-09 14:05:21 -0700

    upgrade image to Fedora Atomic 28

--- kubernetes_magnum/create_template.sh
+++ kubernetes_magnum/create_template.sh
@@ -1,9 +1,10 @@
 FLAVOR="m1.medium"
 MASTER_FLAVOR=$FLAVOR
 DOCKER_VOLUME_SIZE_GB=10
+IMAGE=Fedora-Atomic-28-20180625-private
 
 openstack coe cluster template create --coe kubernetes \
-    --image Fedora-Atomic-27-20180419 \
+    --image $IMAGE \
     --keypair default-keypair \
     --external-network public --fixed-network ${OS_USERNAME}-k8s-network --fixed-subnet ${OS_USERNAME}-k8s-subnet1 --network-driver flannel \
     --flavor $FLAVOR --master-flavor $MASTER_FLAVOR \
